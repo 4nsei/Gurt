5eb65433c3839941ffa933690acd9e19e96bb506
4nsei <cardealerdoesron@gmail.com>
2025-05-10 23:13:31 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+--[[
+    ____            _     _____                     _             
+ / ___|_   _ _ __| |_  | ____|_  _____  ___ _   _| |_ ___  _ __ 
+| |  _| | | | '__| __| |  _| \ \/ / _ \/ __| | | | __/ _ \| '__|
+| |_| | |_| | |  | |_  | |___ >  <  __/ (__| |_| | || (_) | |   
+ \____|\__,_|_|   \__| |_____/_/\_\___|\___|\__,_|\__\___/|_|   
+
+  By: 4nsei
+]]
+
+--ill add it here when im done, yes i have the script and it does work its just still buggy for the replication functions
